6e23c4687b19960189ba7b41155c4712e35af9eb
u0146803 <ella.eycken@kuleuven.be>
2025-04-09 17:33:23 +0200

    initialized feature extraction scripts

---
+++ ella_phd_nlp_project/ella_phd_nlp_code/features/feature_implementation/feature_extraction_audio-based.py
@@ -0,0 +1,57 @@
+"""Extract linguistic features from audio-files.
+The following features are extracted:
+- Fluency
+    - Short Pause
+    - Long Pause
+
+"""
+
+import statistics
+import math
+
+import epitran
+import nltk
+import pandas as pd
+import spacy
+
+from ella_phd_nlp_project.ella_phd_nlp_code.constants import (
+    # AoA_PATH,
+    # CONCRETENESS_PATH,
+    # FREQUENCY_PATH,
+    # DUTCH_ALPHABET,
+    # FAMILIARITY_PATH,
+    # MILTENBURG_MODEL_PATH,
+    # NAME_AGREEMENT_PATH,
+    TEXT_DIR,
+)
+
+# from masterthesisellalaw.data.preprocess_norms import (
+# read_Norms_AoA,
+# read_Norms_concreteness,
+# read_Norms_familiarity,
+# read_Norms_frequency,
+# read_Norms_nameAgreement,
+# )
+
+from ella_phd_nlp_project.ella_phd_nlp_code.features.preliminary_analysis import (
+    read_transcripts,
+)
+
+epi = epitran.Epitran("nld-Latn")
+nlp = spacy.load("nl_core_news_lg")
+
+# linguistic features: language --> Lexical features
+# List:
+# - Lexical Variation
+# - Word finding problems/lexical recovery
+# - Occurrence Frequency/Lexical Content
+# - Psycholinguistic word variables/lexical content
+# - Lexical Access
+# - Complexity
+# - Vocabulary Size
+
+# IDEA: use the Dutch tagger from EVANMILTENBURG
+# https://github.com/evanmiltenburg/Dutch-tagger/tree/master
+# This may take a few minutes. (But once loaded, the tagger is really fast!)
+tagger = PerceptronTagger(load=False)
+tagger.load(MILTENBURG_MODEL_PATH)
---
+++ ella_phd_nlp_project/ella_phd_nlp_code/features/feature_implementation/feature_extraction_text-based.py
@@ -0,0 +1,72 @@
+"""Extract linguistic features from text-files
+The following features are extracted:
+- Semantics:
+    - semantic paraphasias
+- Phonology
+    - phonemic paraphasias
+    - Neologisms
+- Lexical
+    - Number of words
+    - Brunet's Index
+    - Noun Rate
+    - Verb Rate
+    - Adjective Rate
+    - Pronoun Rate
+    - Adverb Rate
+    - Determiner Rate
+    - Conjunction Rate
+    - Preposition Rate
+    - Particle Rate
+    - Content-Function Rate
+- Grammatical
+    - Mean Length of Utterance
+    - Noun-verb Rate
+    - Subordinate Clause
+    - Syntactic Deviation
+- Fluency
+    - Filled Pause
+    - False Start
+    - Word Abandoned
+    - Word Repetition
+
+"""
+
+""" IMPORT STATEMENTS """
+
+import statistics
+import math
+
+import epitran
+import nltk
+import pandas as pd
+import spacy
+
+
+from ella_phd_nlp_project.ella_phd_nlp_code.constants import (
+    # AoA_PATH,
+    # CONCRETENESS_PATH,
+    # FREQUENCY_PATH,
+    # DUTCH_ALPHABET,
+    # FAMILIARITY_PATH,
+    # MILTENBURG_MODEL_PATH,
+    # NAME_AGREEMENT_PATH,
+    TEXT_DIR,
+)
+
+# from masterthesisellalaw.data.preprocess_norms import (
+    # read_Norms_AoA,
+    # read_Norms_concreteness,
+    # read_Norms_familiarity,
+    # read_Norms_frequency,
+    # read_Norms_nameAgreement,
+# )
+
+from ella_phd_nlp_project.ella_phd_nlp_code.features.preliminary_analysis import (
+    read_transcripts,
+)
+
+epi = epitran.Epitran("nld-Latn")
+nlp = spacy.load("nl_core_news_lg")
+
+tagger =
+tagger.load(MILTENBURG_MODEL_PATH)
